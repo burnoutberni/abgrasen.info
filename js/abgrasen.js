c11debb0371626c21cb927630508deb5b238c142
burnoutberni <bernhard.hayden@gmail.com>
2014-11-01 03:14:41 +0100

    merged overview.html with index.html

--- js/abgrasen.js
+++ js/abgrasen.js
@@ -10,14 +10,93 @@ var people = {
   'paul': {'cc': 'CC-BY <a href="http://lutoma.org">lutoma</a>'}
 };
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var showOverview = function() {
+  $('#bauen').hide();
+  $('#overview').show();
+  $('#centeredDiv').show();
+  $('.site-wrapper').animate({opacity: 1}, { duration: 600, queue: false });
+
+  $("#centeredDiv").css("top",($(window).height()-$("#centeredDiv").height())/2);
+  $(".col-md-4").height($(window).height()/3);
+
+  var persons = [];
+
+  //Find all img-files with Apaches index site
+  $.ajax({
+    url: "img/",
+    success: function(data){
+      var i = 0;
+      $(data).find("td > a").each(function(){
+        if($(this).attr("href").indexOf(".jpg") >= 0) {
+          var newPerson = $(this).attr("href").split('.jpg')[0];
+          persons.push(newPerson);
+          i++;
+        }
+      });
+    },
+    complete: function() {
+      var loadedimage = [];
+      var m = 0;
+      for(var k=0; k < 9; k++) {
+        loadedimage[k] = new Image();
+
+        $(loadedimage[k]).attr('src','img/'+persons[k]+'.jpg').load(function() {
+          $('#tile'+m).css('background-image','url('+this.src+')');
+          $('#tile'+m).parent().attr('href','http://'+persons[m]+'.abgrasen.info');
+          $('#tile'+m).animate({opacity:1},'slow');
+          m++;
+        });
+
+      }
+
+
+      setTimeout(function() {
+        $('body').css("background","black");
+        setInterval(function() {
+          shuffle(persons);
+          var j = Math.floor(Math.random() * 9);
+          var loadedimage = new Image();
+          $(loadedimage).attr('src','img/'+persons[j]+'.jpg').load(function() {
+            $('#tile'+j).fadeTo('slow', 0.3, function() {
+              $(this).css('background-image','url("img/'+persons[j]+'.jpg")');
+              $('#tile'+j).parent().attr('href','http://'+persons[j]+'.abgrasen.info');
+            }).fadeTo('slow', 1);
+          });
+        }, 1000);
+      }, 5000);
+    }
+  });
+};
+
 $(document).ready(function() {
   var hashString = $(location).attr('hash').substring(1, $(location).attr('hash').length);
   var ssl = (window.location.href.indexOf('https://') >= 0);
   var domainString = window.location.href.split(ssl ? 'https://' : 'http://')[1].split('.');
 
-  if (domainString.length < 3) {
+  if (domainString.length < 3 || domainString[2] === 'html') {
     // We don't have a name subdomain, redirect to default
-    window.location.replace('http://juli.abgrasen.info');
+    //window.location.replace('http://juli.abgrasen.info');
+    showOverview();
+    return;
   }
 
   var personName = domainString[0];
@@ -61,4 +140,9 @@ $(document).ready(function() {
     $('#cc').html(person['cc']);
     $('#cc').show();
   }  
+});
+
+$(window).on('resize', function(){
+  $("#centeredDiv").css("top",($(window).height()-$("#centeredDiv").height())/2);
+  $(".col-md-4").height($(window).height()/3);
 });
